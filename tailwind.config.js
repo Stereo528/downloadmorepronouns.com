e76bd5518f5d5c3cfa6d33c2c33dee3ef1360d0f
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2023-08-13 15:25:37 -0600

    start header and add dark mode to config
    dark mode to be implemented later

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
     screens: {
       sm: '480px',
@@ -14,7 +15,12 @@ export default {
         'background': "#ebdef2",
         'primary': "#b8b7c2",
         'secondary': "#e1c4f4",
-        'accent': "#8824c6",    
+        'accent': "#8824c6",
+        'dark-text': '#ebdef2',
+        'dark-background': '#030911',
+        'dark-primary': '#b8b7c2',
+        'dark-secondary': '#12051b',
+        'dark-accent': '#cb97ec', 
       },
       fontFamily: {
         'poppins': ['Poppins'],
